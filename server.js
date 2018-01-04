7739d59501772f8949c6dfef571a9904ebdbaf76
Eric Yii <hao_yii0213@hotmail.com>
2018-01-04 12:34:30 -0500

    nodemon

--- server.js
+++ server.js
@@ -1,11 +1,4 @@
-// var express = require('express'),
-//   app = express(),
-//   port = process.env.PORT || 3000;
 
-// app.listen(port);
-
-// console.log('todo list RESTful API server started on: ' + port);
-// process.stdout.write(process.env.PORT);
 var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000,
@@ -32,16 +25,6 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
    });
 
-// app.post("/addname", (req, res) => {
-//  var myData = new User(req.body);
-//  myData.save()
-//  .then(item => {
-//  res.send("item saved to database");
-//  })
-//  .catch(err => {
-//  res.status(400).send("unable to save to database");
-//  });
-// });
 
 
 app.use(function(req, res) {
