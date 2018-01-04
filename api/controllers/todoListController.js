965126ed4b934c576f88b5f93bd4d9ceed4e5af4
Eric Yii <hao_yii0213@hotmail.com>
2018-01-04 09:11:05 -0500

    printing each req method, route and time on console

--- api/controllers/todoListController.js
+++ api/controllers/todoListController.js
@@ -9,7 +9,7 @@ exports.list_all_tasks = function(req, res) {
   Task.find({}, function(err, task) {
     if (err)
       res.send(err);
-    console.log(req.method+" on "+ req.url);
+    console.log(req.method+" on "+ req.url + " at " + new Date().toISOString());
     res.json(task);
   });
 };
